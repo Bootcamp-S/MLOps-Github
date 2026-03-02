2787d00e291d73b984c4ee94b8cb49ef2d6780fd
Bootcamp-S <bootcamp731@gmail.com>
2026-03-02 17:31:56 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import mlflow
 
+def train():
+    mlflow.set_experiment("iris-training")
+
+    data = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target)
+
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    acc = accuracy_score(y_test, model.predict(X_test))
+
+    with mlflow.start_run():
+        mlflow.log_metric("accuracy", acc)
+        mlflow.sklearn.log_model(model, "model")
+
+if __name__ == "__main__":
+    train()
