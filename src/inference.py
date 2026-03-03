68d9d66ac0136ab5e95c4fd5a26d424d738e0be1
Bootcamp-S <bootcamp731@gmail.com>
2026-03-03 16:13:57 +0100

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,1 +1,18 @@
+# src/inference.py
 
+import json
+import numpy as np
+import joblib
+
+def init():
+    global model
+    model = joblib.load("model.pkl")  # Azure ML platziert das Modell hier
+
+def run(data):
+    try:
+        inputs = json.loads(data)["data"]
+        inputs = np.array(inputs)
+        preds = model.predict(inputs)
+        return preds.tolist()
+    except Exception as e:
+        return {"error": str(e)}
